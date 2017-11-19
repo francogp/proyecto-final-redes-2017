d7b6b8b911d2694007dc6d0f9ed51bf3dfa3091f
Franco Gastón Pellegrini <francogpellegrini@gmail.com>
2017-11-19 10:50:35 -0300

    Configs added + Tests

--- src/main/java/ar/edu/unrc/pellegrini/franco/utils/Configs.java
+++ src/main/java/ar/edu/unrc/pellegrini/franco/utils/Configs.java
@@ -1,11 +1,21 @@
 package ar.edu.unrc.pellegrini.franco.utils;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public
 class Configs {
-    private final int distributedArraySize;
-    private final int processQuantity;
+    private final long                distributedArraySize;
+    private final Map< Long, String > hosts;
+    private final int                 processQuantity;
 
     public
     Configs(
@@ -14,21 +24,50 @@ class Configs {
     ) {
         this.distributedArraySize = distributedArraySize;
         this.processQuantity = processQuantity;
+        hosts = new HashMap<>();
     }
 
     public
     Configs( final String configFilePath ) {
-        File configFile = new File(configFilePath);
-        if ( !configFile.exists() ) {
-            throw new IllegalArgumentException("<config file path>.txt does not exists.");
-        }
-        this.distributedArraySize = 0;
-        this.processQuantity = 0;
+        this(new File(configFilePath));
     }
 
     public
-    int getDistributedArraySize() {
+    Configs( final File configFilePath ) {
+        try {
+            // read the json file
+            FileReader reader = new FileReader(configFilePath);
+
+            JSONParser jsonParser = new JSONParser();
+            JSONObject jsonObject = (JSONObject) jsonParser.parse(reader);
+
+            distributedArraySize = (long) jsonObject.get("distributedArraySize");
+            JSONArray hostsInJSON = (JSONArray) jsonObject.get("hosts");
+            if ( hostsInJSON.isEmpty() ) {
+                throw new IllegalArgumentException("wrong host quantity in config file \"" + configFilePath + "\".");
+            }
+            processQuantity = hostsInJSON.size();
+            hosts = new HashMap<>(processQuantity);
+            // take each value from the json array separately
+            for ( Object host : hostsInJSON ) {
+                JSONObject innerObj = (JSONObject) host;
+                hosts.put((Long) innerObj.get("pid"), (String) innerObj.get("address"));
+            }
+        } catch ( IOException e ) {
+            throw new IllegalArgumentException("problems loading \"" + configFilePath + "\".", e);
+        } catch ( ParseException e ) {
+            throw new IllegalArgumentException("wrong json format in config file \"" + configFilePath + "\".", e);
+        }
+    }
+
+    public
+    long getDistributedArraySize() {
         return distributedArraySize;
+    }
+
+    public
+    Map< Long, String > getHosts() {
+        return hosts;
     }
 
     public
---
+++ src/test/java/ar/edu/unrc/pellegrini/franco/utils/ConfigsTest.java
@@ -0,0 +1,27 @@
+package ar.edu.unrc.pellegrini.franco.utils;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+class ConfigsTest {
+    @Test
+    void generalConfig() {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File        file        = new File(classLoader.getResource("ar/edu/unrc/pellegrini/franco/utils/configTest.json").getFile());
+
+        Configs configs = new Configs(file);
+        assertThat(configs.getProcessQuantity(), is(3));
+        assertThat(configs.getDistributedArraySize(), is(10L));
+        Map< Long, String > hosts = configs.getHosts();
+        assertThat(hosts.size(), is(configs.getProcessQuantity()));
+        for ( long i = 1; i <= configs.getProcessQuantity(); i++ ) {
+            String address = "localhost:900" + i;
+            assertThat(hosts.get(i), is(address));
+        }
+    }
+}
