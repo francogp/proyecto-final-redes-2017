39060a8ea45996f00d3651e7d9a658534d1ec49b
Franco Gastón Pellegrini <francogpellegrini@gmail.com>
2017-11-20 01:16:59 -0300

    MsgQueue

---
+++ src/main/java/ar/edu/unrc/pellegrini/franco/utils/MsgQueue.java
@@ -0,0 +1,48 @@
+package ar.edu.unrc.pellegrini.franco.utils;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.function.Function;
+import java.util.logging.Level;
+
+import static java.util.logging.Logger.getLogger;
+
+public
+class MsgQueue< M >
+        implements Runnable {
+
+    private final LinkedBlockingQueue< M > queue;
+    private       Function< M, Boolean >   processMessageFunction;
+    private       boolean                  running;
+
+    /**
+     * @param processMessageFunction M = message to process, and return true if must continue to process further messages.
+     */
+    public
+    MsgQueue(
+            final Function< M, Boolean > processMessageFunction
+    ) {
+        this.processMessageFunction = processMessageFunction;
+        queue = new LinkedBlockingQueue<>();
+    }
+
+    public
+    void enqueue( final M msg ) {
+        queue.add(msg);
+    }
+
+    @Override
+    public
+    void run() {
+        try {
+            running = true;
+            while ( running ) {
+                final M message = queue.take();
+                running = processMessageFunction.apply(message);
+            }
+        } catch ( final InterruptedException ignored ) {
+            //no hacer nada si se interrumpe esperando
+        } catch ( final Error e ) {
+            getLogger(MsgQueue.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
+}
---
+++ src/test/java/ar/edu/unrc/pellegrini/franco/utils/MsgQueueTest.java
@@ -0,0 +1,38 @@
+package ar.edu.unrc.pellegrini.franco.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
+
+class MsgQueueTest {
+    @Test
+    void run() {
+        StringBuilder output = new StringBuilder();
+        MsgQueue< String > msgQueue = new MsgQueue<>(msg -> {
+            if ( msg.equals("end") ) {
+                return false;
+            } else {
+                output.append(msg).append("\n");
+                return true;
+            }
+        });
+        Thread msgQueueThread = new Thread(msgQueue);
+        msgQueueThread.start();
+        msgQueue.enqueue("hola");
+        msgQueue.enqueue("como");
+        msgQueue.enqueue("estas 123");
+        msgQueue.enqueue("end");
+        msgQueue.enqueue("lalalalal error");
+        try {
+            Thread.sleep(10);
+        } catch ( InterruptedException e ) {
+            fail(e);
+        }
+        String finalMsg       = output.toString();
+        String expectedOutput = "hola\ncomo\nestas 123\n";
+        assertThat(finalMsg, is(expectedOutput));
+    }
+
+}
